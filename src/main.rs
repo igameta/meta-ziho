fc4a829535ef5b2d774e0f17cf506a915fef31bb
igameta <igameta0510@outlook.jp>
2025-02-02 10:36:51 +0900

    再生できるようになった

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,22 @@ use chrono::{Local, DateTime};
 use rodio::Decoder;
 
 fn main() {
+    println!("時報起動");
     // 時刻の取得
     let now: DateTime<Local> = Local::now();
     let str_time = now.format("%H時%M分").to_string();
-    print!("{}", str_time);
+    println!("{}", str_time);
 
+    println!("音声合成開始");
     // 音声合成と音声データの加工
-    let jtalk_dict_dir = std::ffi::CString::new("open_jtalk_dic_utf_8").unwrap();
+    let jtalk_dict_dir = std::ffi::CString::new("open_jtalk_dic_utf_8-1.11").unwrap();
     let vvc = VoicevoxCore::new_from_options(AccelerationMode::Auto, 0, true, jtalk_dict_dir.as_c_str()).unwrap();
     let speaker: u32 = 1;
     let sound = vvc.tts_simple(&str_time, speaker).unwrap();
     let cursor = std::io::Cursor::new(sound.as_slice().to_vec());
 
+    println!("音声合成終了");
+    println!("音声再生開始");
     // 生成確認用
     // let mut file = std::fs::File::create("output.wav").unwrap();
     // file.write_all(&sound.as_slice()).unwrap();
